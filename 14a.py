f74bdc243e08ef2ffe70213c8bd716af6a0cb5a0
Daniel Kullmann <daniel.kullmann@sap.com>
2018-03-16 13:00:29 +0100

    Working on day 14, problem 2

---
+++ 14a.py
@@ -0,0 +1,99 @@
+import sys
+
+def twist(ring, start, length):
+    begin = start
+    end = (start+length-1)%len(ring)
+    steps = length/2
+    for s in range(0, steps):
+        temp = ring[begin]
+        ring[begin] = ring[end]
+        ring[end] = temp
+        begin = (begin+1) % len(ring)
+        end = (end-1) % len(ring)
+    return ring
+
+def rounds(input, lengths):
+    skip = 0
+    position = 0
+    for round in range(0, 64):
+        for length in lengths:
+            if length != 1:
+                input = twist(input, position, length)
+            #print position, length, input
+            position = (position + length + skip) % len(input)
+            skip += 1
+    return input
+
+def dense_hash(input):
+    result = []
+    for block_no in range(0,16):
+        block = input[16*block_no:16*(block_no+1)]
+        single_result = 0
+        for entry in block:
+            single_result = single_result ^ entry
+        result.append(single_result)
+    return result
+
+def number2hex(number):
+    result = hex(number)[2:]
+    while len(result) < 2:
+        result = "0" + result
+    return result
+
+def encode(input):
+    return "".join([number2hex(i) for i in input])
+    
+def knot_hash(ring, input):
+    result = rounds(ring, input)
+    return encode(dense_hash(result))
+    
+def hex2bin(hex):
+    value = bin(int(hex, 16))[2:]
+    while len(value) < 128:
+        value = "0" + value
+    return value
+
+input = "amgozmfv"
+#input = "flqrgnkx"
+grid = []
+for row in range(0, 128):
+    data = input+"-"+str(row)
+    data = [ord(s) for s in data] + [17, 31, 73, 47, 23]
+    ring = range(0,256)
+    hash = knot_hash(ring, data)
+    binary = [s for s in hex2bin(hash).replace("0", ".").replace("1", "#")]
+    grid.append(binary)
+
+#for row in grid: print row
+
+last_group = 0
+for y in range(0, 128):
+    for x in range(0, 128):
+        if grid[y][x] == "#":
+            print y, x, grid[y][x], grid[y-1][x]
+            if y > 0 and grid[y-1][x] not in (".", "#"):
+                grid[y][x] = grid[y-1][x]
+                print ">", grid[y][x], grid[y-1][x]
+            #if y < 127 and grid[y+1][x] not in (".", "#"):
+            #    grid[y][x] = grid[y+1][x]
+            if x > 0 and grid[y][x-1] not in (".", "#"):
+                grid[y][x] = grid[y][x-1]
+            #if x < 127 and grid[y][x+1] not in (".", "#"):
+            #    grid[y][x] = grid[y][x+1]
+            if grid[y][x] == "#":
+                last_group += 1
+                grid[y][x] = last_group
+                print "#", grid[y][x] 
+
+#for row in grid: print row
+
+for row in grid[0:8]:
+    for cell in row[0:8]:
+        if cell == ".":
+            sys.stdout.write(".")
+        else:
+            sys.stdout.write(str(cell%10))
+    sys.stdout.write("\n")
+    sys.stdout.flush()
+
+print last_group
