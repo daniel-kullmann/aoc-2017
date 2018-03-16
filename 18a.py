6c1c353dd06014f31f5c86fe2959189178d87c97
Daniel Kullmann <daniel.kullmann@sap.com>
2018-03-16 23:45:58 +0100

    Fixed 18/2

--- 18a.py
+++ 18a.py
@@ -1,7 +1,3 @@
-import re
-
-NUMBER = re.compile(r"^-?\d+$")
-
 def parse(asm):
     program = []
     for line in asm:
@@ -11,7 +7,7 @@ def parse(asm):
 
 
 def get(what, registers):
-    if NUMBER.match(what) != None:
+    if what not in "abcdefghijklmnopqrstuvwxyz":
         return int(what)
     return registers.get(what, 0)
 
@@ -31,28 +27,25 @@ def step(program, state, otherPipe):
     if mnemonic == "snd":
         value = get(command[1], registers)
         otherPipe.append(value)
-        print name, "sent", value
+        #print name, "sent", value
         sendCount += 1
     elif mnemonic == "set":
         registers[command[1]] = get(command[2], registers)
     elif mnemonic == "add":
-        registers[command[1]] = get(command[1], registers) + get(command[2], registers)
+        registers[command[1]] += get(command[2], registers)
     elif mnemonic == "mul":
-        registers[command[1]] = get(command[1], registers) * get(command[2], registers)
+        registers[command[1]] *= get(command[2], registers)
     elif mnemonic == "mod":
-        registers[command[1]] = get(command[1], registers) % get(command[2], registers)
+        registers[command[1]] %= get(command[2], registers)
     elif mnemonic == "rcv":
-        if get(command[1], registers) != 0:
-            if len(pipe) > 0:
-                o = pipe
-                value = pipe[0]
-                registers[command[1]] = value
-                pipe = pipe[1:]
-                print name, "received", value, len(o), len(pipe)
-            else:
-                pc_was_set = True # i.e. just stay at this pc
-                print name, "is waiting"
-                waiting = True
+        if len(pipe) > 0:
+            value = pipe.pop(0)
+            registers[command[1]] = value
+            #print name, "received", value, len(pipe)
+        else:
+            pc_was_set = True # i.e. just stay at this pc
+            #print name, "is waiting"
+            waiting = True
     elif mnemonic == "jgz":
         if get(command[1], registers) > 0:
             pc += get(command[2], registers)
@@ -67,17 +60,17 @@ asm = [s.strip() for s in fh.readlines()]
 fh.close()
 program = parse(asm)
 
-state1 = [0, None, [], {"p": 0}, 0, "0"]
-state2 = [0, None, [], {"p": 1}, 0, "1"]
+state0 = [0, None, [], {"p": 0}, 0, "0"]
+state1 = [0, None, [], {"p": 1}, 0, "1"]
 
 while True:
-    state1 = step(program, state1, state2[2])
-    state2 = step(program, state2, state1[2])
-    if state1[1] == True and state2[1] == True:
+    state0 = step(program, state0, state1[2])
+    state1 = step(program, state1, state0[2])
+    if state0[1] == True and state1[1] == True:
         # both programs wait on each other -> deadlock!
         break
 
+print state0
 print state1
-print state2
-(pc, waiting, pipe, registers, sendCount, name) = state2
+(pc, waiting, pipe, registers, sendCount, name) = state1
 print sendCount
