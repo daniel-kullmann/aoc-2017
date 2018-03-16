82951d1ff7118f51412b605ea01dadb6b2ab80e6
Daniel Kullmann <daniel.kullmann@sap.com>
2018-03-16 19:55:33 +0100

    Day 19, problem 1 solved

---
+++ 19.py
@@ -0,0 +1,77 @@
+fh = open("19.txt")
+maze = [s for s in fh.readlines()]
+fh.close()
+y = 0
+x = maze[0].index("|")
+height = len(maze)
+width = len(maze[0])
+direction = "s"
+letters = []
+
+while True:
+    spot = maze[y][x]
+    print y, x, spot, direction
+    if spot not in "+-| ":
+        letters.append(spot)
+    if direction == "s":
+        if spot not in ("+", " "):
+            y += 1
+        elif spot == "+":
+            if x>0 and maze[y][x-1] != " ":
+                direction = "w"
+                x -= 1
+            elif x<width-1 and maze[y][x+1] != " ":
+                direction = "e"
+                x += 1
+            else:
+                raise Exception("state " + str((direction,x,y,spot)))
+        else:
+            break
+            raise Exception("state " + str((direction,x,y,spot)))
+    elif direction == "n":
+        if spot not in ("+", " "):
+            y -= 1
+        elif spot == "+":
+            if x>0 and maze[y][x-1] != " ":
+                direction = "w"
+                x -= 1
+            elif x<width-1 and maze[y][x+1] != " ":
+                direction = "e"
+                x += 1
+            else:
+                raise Exception("state " + str((direction,x,y,spot)))
+        else:
+            raise Exception("state " + str((direction,x,y,spot)))
+    elif direction == "e":
+        if spot not in ("+", " "):
+            x += 1
+        elif spot == "+":
+            if y>0 and maze[y-1][x] != " ":
+                direction = "n"
+                y -= 1
+            elif y<height-1 and maze[y+1][x] != " ":
+                direction = "s"
+                y += 1
+            else:
+                raise Exception("state " + str((direction,x,y,spot)))
+        else:
+            raise Exception("state " + str((direction,x,y,spot)))
+    elif direction == "w":
+        if spot not in ("+", " "):
+            x -= 1
+        elif spot == "+":
+            if y>0 and maze[y-1][x] != " ":
+                direction = "n"
+                y -= 1
+            elif y<height-1 and maze[y+1][x] != " ":
+                direction = "s"
+                y += 1
+            else:
+                raise Exception("state " + str((direction,x,y,spot)))
+        else:
+            raise Exception("state " + str((direction,x,y,spot)))
+    else:
+        raise Exception("state " + str((direction,x,y,spot)))
+        
+
+print "".join(letters)
